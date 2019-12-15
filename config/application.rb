8cdcc261fd5da0b91dfb1c7343e9b9d1388b1a3d
cristianbgp <cristian.granda.pastor@gmail.com>
2019-12-15 09:26:19 -0500

    Setup rack-cors

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
@@ -33,5 +33,12 @@ module MySpotifyApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins "*"
+        resource "*", headers: :any, methods: [:get]
+      end
+    end
   end
 end
