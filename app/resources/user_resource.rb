ebc4a4197d5e6ef1db184643e76d27bedc0bd4cb
cristianbgp <cristian.granda.pastor@gmail.com>
2019-12-28 18:56:41 -0500

    Add full name attribute

--- app/resources/user_resource.rb
+++ app/resources/user_resource.rb
@@ -1,4 +1,8 @@
 class UserResource < JSONAPI::Resource
-  attributes :first_name, :last_name, :email
+  attributes :first_name, :last_name, :full_name, :email
   has_many :payments
+
+  def full_name
+    "#{@model.first_name} #{@model.last_name}"
+  end
 end
